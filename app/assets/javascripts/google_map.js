e26efe2ce197e33f79c4840d3a14bfe64345858f
Carl-Philip <callea2@gmail.com>
2016-06-22 15:58:22 +0200

    Added geolocation Updated google_map.js The user will now be asked for permission to grab their location information and display it on the map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,6 +1,32 @@
 function initMap() {
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
-    center: {lat: 59.3293235, lng: 18.0685808}, zoom: 8
+    center: {lat: 59.3293235, lng: 18.0685808},
+    zoom: 8
   });
-}
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here.');
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
