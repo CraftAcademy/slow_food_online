bf49d5953aa6a8acc4434bda48185af7c3d4384c
Raoul DIFFOUO <diraulo@gmail.com>
2016-06-22 14:22:41 +0200

    fix issue with navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require turbolinks
-//= require foundation
 //= require_tree .
 
-
-
-$(function(){ $(document).foundation(); });
 //= require turbolinks
